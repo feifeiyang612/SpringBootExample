f8ef5e14d0e649eff0fdfeda34d61fc402b1da10
yangfei <644752026@qq.com>
2019-11-15 12:34:16 +0800

    SpringBootHelloWord

---
+++ SpringBootHelloWord/src/main/java/com/yf/springboot/helloword/SpringBootHelloWord/SpringBootHelloWordApplication.java
@@ -0,0 +1,13 @@
+package com.yf.springboot.helloword.SpringBootHelloWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWordApplication.class, args);
+	}
+
+}
---
+++ SpringBootHelloWord/src/test/java/com/yf/springboot/helloword/SpringBootHelloWord/SpringBootHelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yf.springboot.helloword.SpringBootHelloWord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
