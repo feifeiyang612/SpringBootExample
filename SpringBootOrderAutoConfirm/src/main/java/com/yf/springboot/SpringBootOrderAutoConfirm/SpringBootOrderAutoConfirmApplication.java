9a67ca73173c73c9938a966587b522166878a6c1
yangfei <644752026@qq.com>
2019-11-19 18:27:46 +0800

    update

--- SpringBootOrderAutoConfirm/src/main/java/com/yf/springboot/SpringBootOrderAutoConfirm/SpringBootOrderAutoConfirmApplication.java
+++ SpringBootOrderAutoConfirm/src/main/java/com/yf/springboot/SpringBootOrderAutoConfirm/SpringBootOrderAutoConfirmApplication.java
@@ -1,13 +1,43 @@
 package com.yf.springboot.SpringBootOrderAutoConfirm;
 
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootOrderAutoConfirmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootOrderAutoConfirmApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOrderAutoConfirmApplication.class, args);
+    }
+
+    @Bean
+    public Docket restApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+//                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .apis(RequestHandlerSelectors.basePackage("com.yf.springboot.SpringBootOrderAutoConfirm.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger测试")
+                .description("【订单30分钟不支付自动取消功能】实例swagger接口")
+                .version("0.0.1")
+                .build();
+    }
+
 
 }
--- SpringBootOrderAutoConfirm/src/main/java/com/yf/springboot/SpringBootOrderAutoConfirm/controller/OrderController.java
+++ SpringBootOrderAutoConfirm/src/main/java/com/yf/springboot/SpringBootOrderAutoConfirm/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.yf.springboot.SpringBootOrderAutoConfirm.controller;
 
 import com.yf.springboot.SpringBootOrderAutoConfirm.service.DelayedOrder;
 import com.yf.springboot.SpringBootOrderAutoConfirm.service.OrderService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/order")
+@Api(value = "/index/jepCalculate")
 public class OrderController {
 
     @Autowired
