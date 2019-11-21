733f845415736fac891e91159f000aaf0895f05f
yangfei <644752026@qq.com>
2019-11-21 13:32:00 +0800

    修改

--- SpringBootOrderAutoConfirm/src/main/java/com/yf/springboot/SpringBootOrderAutoConfirm/controller/OrderController.java
+++ SpringBootOrderAutoConfirm/src/main/java/com/yf/springboot/SpringBootOrderAutoConfirm/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.yf.springboot.SpringBootOrderAutoConfirm.controller;
 import com.yf.springboot.SpringBootOrderAutoConfirm.service.DelayedOrder;
 import com.yf.springboot.SpringBootOrderAutoConfirm.service.OrderService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/order")
-@Api(value = "/index/jepCalculate")
+@Api(value = "", tags = "测试接口")
 public class OrderController {
 
     @Autowired
     OrderService orderService;
 
+    @ApiOperation(value = "创建订单")
     @GetMapping("/create")
     public String createOrder(@RequestParam("orderId") String orderId) {
         DelayedOrder order = orderService.createOrder(orderId);
         return order.getValue();
     }
 
+    @ApiOperation(value = "支付订单")
     @GetMapping("/pay")
     public Boolean payOrder(@RequestParam("orderId") String orderId) {
         boolean ret = orderService.payOrder(orderId);
